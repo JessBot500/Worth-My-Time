2ec8183dc78c283e53f3f4da85a43455995b6cdb
jesusnboone <jesusnboone@gmail.com>
2020-07-03 20:35:53 -0500

    trying to make a function using tmdb api

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+function movie(){
+
+    var API = "2215e66d3770fa7ff283fdf766c88f8c"
+    var title = document.querySelector('#movie-title').value;
+
+    fetch ("https://api.themoviedb.org/3/search/movie?api_key="
+    + API + "&query=" + title)
+    .then(function(response) {return response.json()})
+    .then(function(response) {
+    
+
+    var id = (response.results[0].id);
+    console.log(id);
+
+    
+    fetch ("https://api.themoviedb.org/3/movie/"
+    + id
+    + "?api_key="
+    + API )
+
+    .then(details => details.json())
+    .then(console.log);
+
+
+    })
+}
