59914426ed85e0baa69afc5c17bc35cba3941018
e-leal <leal.elyssa03@gmail.com>
2020-07-06 17:52:01 -0500

    static info to display previous search data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+var prevSearchObj = {
+    Type: "Both",
+    Genre: "Action",
+    Actor: "None",
+    RunningTime: 120,
+    Rating: 75
+}
+
+function loadPrevSearch(){
+    if(prevSearchObj.Type === "Both"){
+        document.getElementById("prevType").innerHTML = "Movie / Show";
+    }
+    else{
+        document.getElementById("prevType").innerHTML = prevSearchObj.Type;
+    }
+    document.getElementById("prevGenre").innerHTML = prevSearchObj.Genre;
+    document.getElementById("prevActor").innerHTML = prevSearchObj.Actor;
+    document.getElementById("prevTime").innerHTML = prevSearchObj.RunningTime + " mins";
+    document.getElementById("prevRating").innerHTML = prevSearchObj.Rating + "%";
+}
+
 function requiredChecbox(){
 
     var requiredCheckboxes = $(':checkbox[required]');
@@ -12,7 +33,7 @@ function requiredChecbox(){
             requiredCheckboxes.attr('required', 'required');
         }
     });
-
 }
 
 requiredChecbox();
+loadPrevSearch();
