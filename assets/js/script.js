25b1f1f23607149a5377416b31bd08fdd24156c5
jesusnboone <jesusnboone@gmail.com>
2020-07-06 13:39:42 -0500

    updated my files based on Elyssa's code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+function requiredChecbox(){
+
+    var requiredCheckboxes = $(':checkbox[required]');
+
+    requiredCheckboxes.change(function(){
+
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        }
+
+        else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+
+}
+
+requiredChecbox();
+
 
 function movie(){
 
@@ -53,4 +72,11 @@ function movie(){
 
     })
     })
+}
+
+
+function topFive (){
+
+
+    
 }
