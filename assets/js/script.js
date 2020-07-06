1d1644d206e53303f50a3887eef1abeef745325a
e-leal <leal.elyssa03@gmail.com>
2020-07-05 22:40:00 -0500

    require at least 1 checkbox to be filled - added jquery function

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function requiredChecbox(){
+
+    var requiredCheckboxes = $(':checkbox[required]');
+
+    requiredCheckboxes.change(function(){
+
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        }
+
+        else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+
+}
+
+requiredChecbox();
