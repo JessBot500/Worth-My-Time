219f64e50ca6584808ea90c6ff1e3b80c165814d
e-leal <leal.elyssa03@gmail.com>
2020-07-06 22:48:38 -0500

    display single select movie/show view with static data object

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ var prevSearchObj = {
     RunningTime: 120,
     Rating: 75
 }
+var currentMovie = {
+    title: "Shrek",
+    type: "Movie",
+    genre: "Animated",
+    synopsis: "It ain't easy bein' green -- especially if you're a likable (albeit smelly) ogre named Shrek. On a mission to retrieve a gorgeous princess from the clutches of a fire-breathing dragon, Shrek teams up with an unlikely compatriot -- a wisecracking donkey.",
+    runningTime: 90,
+    posterURL: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg",
+    rating: "56%"
+}
 
 function loadPrevSearch(){
     if(prevSearchObj.Type === "Both"){
@@ -50,6 +59,20 @@ function requiredChecbox(){
 //     })
 // };
 
+function switchSingleView(){
+    console.log("we clicked the button");
+    if($("#leftView").css("display") != "none"){
+        $("#leftView").css("display", "none");
+        $("#rightView").css("display", "none");
+        $("#singleView").css("display", "block");
+        $("#moviePoster").attr('src', currentMovie.posterURL);
+        document.getElementById("singleRating").innerHTML = currentMovie.rating;
+        document.getElementById("movieTitle").innerHTML = currentMovie.title;
+        document.getElementById("runningTime").innerHTML = currentMovie.runningTime + " minutes";
+        document.getElementById("synopsis").innerHTML = currentMovie.synopsis;
+    }
+
+}
 
 
 requiredChecbox();
