4a8ff79064a52446645a95cbc763743fccfff34c
jesusnboone <jesusnboone@gmail.com>
2020-07-07 21:11:45 -0500

    merged Develop branch with Jesus branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,77 @@
+  
+//static previous search from user data
+var prevSearchObj = {
+    Type: "Both",
+    Genre: "Action",
+    Actor: "None",
+    RunningTime: 120,
+    Rating: 75
+}
+
+// static movie selection data object
+var currentMovie = {
+    title: "Shrek",
+    type: "Movie",
+    genre: "Animated",
+    synopsis: "It ain't easy bein' green -- especially if you're a likable (albeit smelly) ogre named Shrek. On a mission to retrieve a gorgeous princess from the clutches of a fire-breathing dragon, Shrek teams up with an unlikely compatriot -- a wisecracking donkey.",
+    runningTime: 90,
+    posterURL: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg",
+    rating: "56%"
+}
+
+function loadPrevSearch(){
+    if(prevSearchObj.Type === "Both"){
+        document.getElementById("prevType").innerHTML = "Movie / Show";
+    }
+    else{
+        document.getElementById("prevType").innerHTML = prevSearchObj.Type;
+    }
+    document.getElementById("prevGenre").innerHTML = prevSearchObj.Genre;
+    document.getElementById("prevActor").innerHTML = prevSearchObj.Actor;
+    document.getElementById("prevTime").innerHTML = prevSearchObj.RunningTime + " mins";
+    document.getElementById("prevRating").innerHTML = prevSearchObj.Rating + "%";
+}
+
+function requiredChecbox(){
+
+    var requiredCheckboxes = $(':checkbox[required]');
+
+    requiredCheckboxes.change(function(){
+
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        }
+
+        else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+}
+
+
+
+
+function switchSingleView(){
+    console.log("we clicked the button");
+    if($("#leftView").css("display") != "none"){
+        $("#leftView").css("display", "none");
+        $("#rightView").css("display", "none");
+        $("#singleView").css("display", "block");
+        $("#moviePoster").attr('src', currentMovie.posterURL);
+        if (current)
+        document.getElementById("singleRating").innerHTML = currentMovie.rating;
+        document.getElementById("movieTitle").innerHTML = currentMovie.title;
+        document.getElementById("runningTime").innerHTML = currentMovie.runningTime + " minutes";
+        document.getElementById("synopsis").innerHTML = currentMovie.synopsis;
+    }
+
+}
+
+
+requiredChecbox();
+loadPrevSearch();
+// searchExpand();
+
 function requiredChecbox(){
 
     var requiredCheckboxes = $(':checkbox[required]');
@@ -171,33 +245,3 @@ function search() {
 })}
 
 topFive();
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
